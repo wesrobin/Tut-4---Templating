787dffde1aef3b1f40aa27d48e6e6e40d6e62fb8
wesley <wesley@Wesley-pc>
2013-05-07 13:38:30 +0200

    Initial commit from netbeans, with Type, Crypt_Traits and main classes

---
+++ Tut 4 - Templating/Crypt_Traits.h
@@ -0,0 +1,19 @@
+/* 
+ * File:   Crypt_Traits.h
+ * Author: wesley
+ *
+ * Created on 07 May 2013, 1:23 PM
+ */
+
+#ifndef CRYPT_TRAITS_H
+#define	CRYPT_TRAITS_H
+
+#include "Type.h"
+
+template <typename grouping, typename packing, typename cipher> class Crypt_Traits {
+    
+};
+
+
+#endif	/* CRYPT_TRAITS_H */
+
