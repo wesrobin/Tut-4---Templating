2c7c91e50f59e555a1c4dfc24083bab5a815d992
wesley <wesley@Wesley-pc>
2013-05-07 13:39:41 +0200

    Actual commit, last one was just Crypt_Traits.h

---
+++ Tut 4 - Templating/Type.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   Type.h
+ * Author: wesley
+ *
+ * Created on 07 May 2013, 1:19 PM
+ */
+
+#ifndef TYPE_H
+#define	TYPE_H
+
+class packing;
+class no_packing;
+
+class grouping;
+class no_grouping;
+
+class caesar;
+class vigenere;
+class XOR;
+
+#endif	/* TYPE_H */
+
---
+++ Tut 4 - Templating/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: wesley
+ *
+ * Created on 07 May 2013, 1:10 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
